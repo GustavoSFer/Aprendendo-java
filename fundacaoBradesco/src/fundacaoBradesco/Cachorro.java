0fc0a20f475d1eae6c353594620613780cb07a05
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-10-18 19:12:59 -0300

    aprendendo java

---
+++ fundacaoBradesco/src/fundacaoBradesco/Cachorro.java
@@ -0,0 +1,10 @@
+package fundacaoBradesco;
+
+public class Cachorro {
+	int tamanho;
+	String raca;
+	
+	void latir() {
+		System.out.println("Au au!!!");
+	}
+}
---
+++ fundacaoBradesco/src/fundacaoBradesco/CachorroTeste.java
@@ -0,0 +1,14 @@
+package fundacaoBradesco;
+
+public class CachorroTeste {
+
+	public static void main(String[] args) {
+		Cachorro slinky = new Cachorro();
+		slinky.raca = "Vira-lata";
+		slinky.tamanho = 30;
+		
+		slinky.latir();
+
+	}
+
+}
