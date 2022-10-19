119c3ae2966b18626c2d9ed4787ada06c9cfad8a
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-10-19 14:26:48 -0300

    Criando classes e heranças

---
+++ fundacaoBradescoInicio/src/br/com/heranca/Animal.java
@@ -0,0 +1,18 @@
+package br.com.heranca;
+
+public class Animal {
+	String nome;
+	double peso;
+	String comida;
+	
+	void comer() {
+		System.out.println("Animal esta comendo!");
+	}
+	
+	void dormir() {
+		System.out.println("Animal foi colocado para dormir!");
+	}
+	void movimentar() {
+		System.out.println("Animal esta se movimentando!!!!");
+	}
+}
